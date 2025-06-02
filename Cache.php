66b078c861d05fefd3c2d7c47c29f8804e2d2532
Dave Humphrey <dave@uesp.net>
2025-06-02 18:07:13 -0400

    turn on filecache

--- Cache.php
+++ Cache.php
@@ -10,14 +10,19 @@ $wgSessionsInObjectCache = true;
 $wgObjectCacheSessionExpiry = 100000;
 $wgMemCachedServers = array($UESP_SERVER_MEMCACHED . ":11000");
 
+$wgFileCacheDirectory = "/mnt/uesp/filecache";
+$wgUseFileCache = true;
+
 if ($uespIsDev)
 {
+	$wgUseFileCache = false;
 	$wgCacheDirectory = "/home/uesp/cache/dev";
 	$wgMemCachedServers = array($UESP_SERVER_BACKUP1 . ":11000");
 	$wgCookiePrefix = "uesp_net_wikidev";
 }
 else
 {
+	$wgFileCacheDirectory = "/mnt/uesp/filecache" . $uespLanguageSuffix;
 	$wgCacheDirectory = "/home/uesp/cache/" . $uespLanguageSuffix;
 	$wgSquidMaxage = 86400;
 	$wgSquidServers = array($UESP_SERVER_SQUID1);
